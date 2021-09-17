be49ebd59493cc7781c2eef33b59435ecdf81071
Cyrille Corpet <cyrille@bayesimpact.org>
2021-09-17 14:57:43 +0200

    [Demos] Show demos from Github deployments in Github statuses. (#93)

--- bin/create_demo_statuses.py
+++ bin/create_demo_statuses.py
@@ -7,11 +7,17 @@ It is available in bayesimpact org under the Slack context.
 """
 
 import argparse
+import logging
 import os
 from os import path
-from typing import Optional, Sequence
+import time
+import typing
+from typing import Optional, Sequence, Set
 
 import requests
+
+if typing.TYPE_CHECKING:
+    import create_demo_statuses_types as types
 
 _STATUSES_URL = (
     f'https://api.github.com/repos/{os.getenv("CIRCLE_PROJECT_USERNAME")}/'
@@ -19,20 +25,110 @@ _STATUSES_URL = (
 _GITHUB_TOKEN = os.getenv('GITHUB_TOKEN', '')
 _DEMO_CONTEXT_PREFIX = 'bayesimpact/demo-'
 
+_DEPLOYMENTS_GRAPHQL_QUERY = '''query($repo: String!, $owner: String!, $prNumber: Int!) {
+  repository(name: $repo, owner: $owner) {
+    pullRequest(number: $prNumber) {
+      timelineItems(last: 10, itemTypes: [DEPLOYED_EVENT]) {
+        nodes {
+          ...on DeployedEvent {
+            deployment {
+              description
+              state
+              latestStatus {
+                environmentUrl
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}'''
 
-def create_demo_status(name: str, url: str) -> None:
+
+_USELESS_DEPLOYMENT_STATES: Set['types._DeploymentState'] = {
+    'INACTIVE',
+}
+_READY_DEPLOYMENT_STATES: Set['types._DeploymentState'] = {
+    'ACTIVE',
+}
+_FAILED_DEPLOYMENT_STATES: Set['types._DeploymentState'] = {
+    'ABANDONED',
+    'DESTROYED',
+    'ERROR',
+    'FAILURE',
+}
+_WAITING_DEPLOYMENT_STATES: Set['types._DeploymentState'] = {
+    'PENDING',
+    'QUEUED',
+    'IN_PROGRESS',
+    'WAITING',
+}
+
+
+def create_demo_status(name: str, url: Optional[str]) -> None:
     """Create a Github status for the given demo."""
 
+    state = 'success' if url else 'failure'
     response = requests.post(_STATUSES_URL, headers={
         'Accept': 'application/vnd.github.machine-man-preview+json',
         'Authorization': f'token {_GITHUB_TOKEN}',
     }, json={
-        'state': 'success',
+        'state': state,
         'context': f'{_DEMO_CONTEXT_PREFIX}{name}',
         'description': f'{name.capitalize()} demo is ready',
-        'target_url': url,
-    })
+    } | ({'target_url': url} if url else {}))
     response.raise_for_status()
+
+
+def wait_for_deployment_urls(
+        deployments: Set[str], max_retry: int = 10) -> dict[str, Optional[str]]:
+    if not deployments:
+        return {}
+    deployments = set(deployments)
+    owner = os.getenv('CIRCLE_PROJECT_USERNAME')
+    pr_number = os.getenv('CIRCLE_PR_NUMBER')
+    repo = os.getenv('CIRCLE_PROJECT_REPONAME')
+    token = os.getenv('GITHUB_TOKEN')
+    if not owner or not repo or not pr_number or not token:
+        return {}
+    result: dict[str, Optional[str]] = {}
+    for unused_ in range(max_retry):
+        response = requests.post('https://api.github.com/graphql', json={
+            'query': _DEPLOYMENTS_GRAPHQL_QUERY,
+            'variables': {
+                'owner': owner,
+                'prNumber': int(pr_number),
+                'repo': repo,
+            },
+        }, headers={'Authorization': f'token {token}'})
+        response.raise_for_status()
+        response_data: 'types._Response' = response.json()
+        for event in response_data.get('data', {}).get('repository', {}).\
+                get('pullRequest', {}).get('timelineItems', {}).get('nodes', []):
+            deployment = event.get('deployment', {})
+            name = deployment.get('description', '').lower()
+            if name not in deployments:
+                continue
+            state = deployment.get('state')
+            if not state or state in _USELESS_DEPLOYMENT_STATES:
+                continue
+            if state in _FAILED_DEPLOYMENT_STATES:
+                result[name] = None
+            if state in _WAITING_DEPLOYMENT_STATES:
+                break
+            if state in _READY_DEPLOYMENT_STATES:
+                url = deployment.get('latestStatus', {}).get('environmentUrl')
+                if not url:
+                    raise ValueError('Got a ready deployment without a URL...')
+                result[name] = url
+        else:
+            deployments -= set(result)
+            if not deployments:
+                return result
+        time.sleep(2)
+    logging.warning('Could not find deployment URLs for "%s"', '", "'.join(deployments))
+    return result
 
 
 def main(string_args: Optional[Sequence[str]] = None) -> None:
@@ -52,12 +148,15 @@ def main(string_args: Optional[Sequence[str]] = None) -> None:
         Each file should be named with the demo name (e.g. "frontend")
         and contain one line with the demo's URL.
     ''')
+    parser.add_argument('--deployment', dest='deployments', action='append', help='''
+        Wait for GitHub deployment(s) to be ready, and link their URL''')
     args = parser.parse_args(string_args)
-    demo_urls: dict[str, str] = dict(args.demo_url or [])
+    demo_urls: dict[str, Optional[str]] = dict(args.demo_url or [])
     if args.directory:
         for filename in os.listdir(args.directory):
             with open(path.join(args.directory, filename)) as file:
                 demo_urls[filename] = file.read().strip()
+    demo_urls |= wait_for_deployment_urls({d.lower() for d in args.deployments})
     for name, url in demo_urls.items():
         create_demo_status(name, url)
 
